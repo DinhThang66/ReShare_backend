e774bb2286930349a528a0d90d5e2e0cc199afcc
DinhThang66 <dinhvanthang06062002@gmail.com>
2025-05-20 13:41:57 +0700

    update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import commentRoutes from './routes/comment.route.js'
 
 // App config
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
